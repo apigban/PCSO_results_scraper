b036d051ee17ff132add026d57a05d635897ecbc
Alain Igban <apigban@gmail.com>
2018-10-08 20:38:14 +0800

    Added __VIEWSTATEGENERATOR to extractor function

--- Extraction/extractor.py
+++ Extraction/extractor.py
@@ -18,25 +18,53 @@ def get_page():
     :return:
     """
     response = requests.get(uri, headers=html_headers)
-    print(response.content)
+    #print(response.content)
     page = html.fromstring(response.content)
-    print(page)
+    #print(page)
     return page
 
 
 def extract_viewstate(raw_html):
     """
     locates @id="__VIEWSTATE" and returns the __VIEWSTATE value
+    locates @id="__VIEWSTATEGENERATOR" and returns the __VIEWSTATEGENERATOR value
     len(viewstate) as of 20181008 is 5680
+    len(viewstategenerator) as of 20181008 is 12
     :rtype: htmlElement object
     """
     viewstate = str(raw_html.xpath('//input[@id="__VIEWSTATE"]/@value'))
-    print(viewstate)
-    print(type(viewstate))
-    print(len(viewstate))
-    return viewstate
+    viewstate_gen = str(raw_html.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value'))
+    #print(viewstate)
+    #print(type(viewstate))
+    #print(len(viewstate))
+
+    #print(viewstate_gen)
+    #print(type(viewstate_gen))
+    #print(len(viewstate_gen))
+
+    return viewstate, viewstate_gen
+
+def init_formfield():
+
+    formfield = {
+        "__VIEWSTATE": viewstate,
+        "__VIEWSTATEGENERATOR": viewstate_gen,
+        "ctl00$ctl00$cphContainer$cpContent$ddlStartMonth": "",
+        "ctl00$ctl00$cphContainer$cpContent$ddlStartDate": "",
+        "ctl00$ctl00$cphContainer$cpContent$ddlStartYear": "",
+        "ctl00$ctl00$cphContainer$cpContent$ddlEndMonth": "",
+        "ctl00$ctl00$cphContainer$cpContent$ddlEndDay": "",
+        "ctl00$ctl00$cphContainer$cpContent$ddlEndYear": "",
+        "ctl00$ctl00$cphContainer$cpContent$ddlSelectGame": "",
+        "ctl00$ctl00$cphContainer$cpContent$btnSearch": ""
+
+
+    }
 
 
 raw_html = get_page()
 
-extract_viewstate(raw_html)
+viewstate, viewstate_gen = extract_viewstate(raw_html)
+
+
+print(viewstate,viewstate_gen)
