fc799a4340e8b7f0c64a84ec537caae04a2751dd
Alain Igban <apigban@gmail.com>
2018-10-11 19:32:48 +0800

    Module for writing to Postgresql DB

---
+++ Extraction/db_writer_postgres.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.7
+
+import re
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relation
+from Extraction import db_config
+
+Base = declarative_base()
+engine = create_engine(
+    f'postgresql://{db_config.username}:{db_config.password}@{db_config.host}:{db_config.port}/{db_config.database}',
+    echo=True)
+
+
+class Combination(Base):
+    __tablename__ = 'lotto'
+
+    id = Column('id', Integer, primary_key=True)
+    datedrawn = Column('date', String)
+    game = Column('game', String)
+    game_result = Column('result', String)
+    jackpot = Column('jackpot', Integer)
+    winners = Column('winners', Integer)
+
+
+def db_commit(parsed_table):
+    """
+    get draw information from passed list
+    initialize db engine, commit drawinfo from parsed list
+    List Structure: [
+        ELEMENT 0: ['LOTTO GAME', 'COMBINATIONS', 'DRAW DATE', 'JACKPOT', 'WINNERS'],
+        ELEMENT 1: ['Megalotto 6/45', '41-05-21-31-45-03', '10/8/2018', '56,250,597.00', '0'],
+                    ...
+                    ]
+    """
+
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+
+    for element in range(1, len(parsed_table)):
+        draw = Combination()
+        session = Session()
+        # print(type(parsed_table[element][0]))
+
+        draw.datedrawn = parsed_table[element][2]
+        draw.game = parsed_table[element][0]
+        draw.game_result = parsed_table[element][1]
+        draw.jackpot = int(float(re.sub('[, ]', '', parsed_table[element][3])))
+        draw.winners = int(parsed_table[element][4])
+
+        session.add(draw)
+        session.commit()
+
+        session.close()
+
+
+if __name__ == '__main__':
+    print('Module Executed Independently.')
